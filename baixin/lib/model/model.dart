f1562dd1f294e086bf6eac3beb99009c3e637d5d
lujunyu <toplu1015@163.com>
2019-03-12 22:20:29 +0800

    android bug

--- baixin/lib/model/model.dart
+++
@@ -1,9 +0,0 @@
-
-export 'package:flutter_button/model/banner.dart';
-export 'package:flutter_button/model/kingkong.dart';
-export 'package:flutter_button/model/product.dart';
-export 'package:flutter_button/model/crowdfund.dart';
-export 'package:flutter_button/model/sub_category.dart';
-export 'package:flutter_button/model/find.dart';
-export 'package:flutter_button/model/search.dart';
-export 'package:flutter_button/model/beautyimage.dart';
