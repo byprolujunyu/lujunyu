e3b193d0fc6ce9728609a60cd5aaa2b21f134b36
lujunyu <toplu1015@163.com>
2019-03-14 09:29:42 +0800

    修改

--- baixin/lib/detail/detail_page.dart
+++ baixin/lib/detail/detail_page.dart
@@ -187,97 +187,105 @@ class _SelectCountWidgetState extends State<SelectCountWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(20.0),
-          height: ScreenUtil().setHeight(150),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(1.0),
-                child: Text(
-                  '购买数量',
+    return WillPopScope(
+      child: Container(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20.0),
+            height: ScreenUtil().setHeight(150),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(1.0),
+                  child: Text(
+                    '购买数量',
+                  ),
                 ),
-              ),
-              Container(
-                child: Divider(),
-              ),
-              Container(
-                child: Row(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () => {_dix_count()},
-                      child: Container(
-                        width: ScreenUtil().setWidth(50),
-                        height: ScreenUtil().setHeight(50),
-                        decoration: BoxDecoration(border: _getRemoveBtBorder()),
-                        child:
-                            Icon(Icons.remove, color: _getRemovebuttonColor()),
-                      ),
-                    ),
-                    Container(
-                        alignment: Alignment.center,
-                        width: ScreenUtil().setWidth(50),
-                        height: ScreenUtil().setHeight(50),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: KColorConstant.cartItemCountTxtColor,
-                                width: 1)),
-                        child: Text(
-                          '${count}',
-                          style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: KColorConstant.cartItemCountTxtColor),
-                        )),
-                    GestureDetector(
-                      onTap: () => {_add_count()},
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: ScreenUtil().setWidth(50),
-                        height: ScreenUtil().setHeight(50),
-                        decoration: BoxDecoration(border: _getAddBtBorder()),
-                        child: Icon(Icons.add, color: _getAddbuttonColor()),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 10.0),
-                      width: ScreenUtil().setWidth(210),
-                      height: ScreenUtil().setHeight(60),
-                      alignment: Alignment.center,
-                      color: Colors.green,
-                      child: InkWell(
-                        onTap: () {
-                          Map m = widget.map;
-                          _add(new User(
-                              name: m['goodsName'],
-                              image: m['image'],
-                              count: count,
-                              price: m['presentPrice']));
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext ctx) {
-                                return CartIosPage();
-                              },
-                            ),
-                          );
-                        },
-                        child: Text(
-                          '确认加入购物车',
-                          style: TextStyle(color: Colors.white),
+                Container(
+                  child: Divider(),
+                ),
+                Container(
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () => {_dix_count()},
+                        child: Container(
+                          width: ScreenUtil().setWidth(50),
+                          height: ScreenUtil().setHeight(50),
+                          decoration:
+                              BoxDecoration(border: _getRemoveBtBorder()),
+                          child: Icon(Icons.remove,
+                              color: _getRemovebuttonColor()),
                         ),
                       ),
-                    ),
-                  ],
+                      Container(
+                          alignment: Alignment.center,
+                          width: ScreenUtil().setWidth(50),
+                          height: ScreenUtil().setHeight(50),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: KColorConstant.cartItemCountTxtColor,
+                                  width: 1)),
+                          child: Text(
+                            '${count}',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: KColorConstant.cartItemCountTxtColor),
+                          )),
+                      GestureDetector(
+                        onTap: () => {_add_count()},
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: ScreenUtil().setWidth(50),
+                          height: ScreenUtil().setHeight(50),
+                          decoration: BoxDecoration(border: _getAddBtBorder()),
+                          child: Icon(Icons.add, color: _getAddbuttonColor()),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 10.0),
+                        width: ScreenUtil().setWidth(210),
+                        height: ScreenUtil().setHeight(60),
+                        alignment: Alignment.center,
+                        color: Colors.green,
+                        child: InkWell(
+                          onTap: () {
+                            Map m = widget.map;
+                            _add(new User(
+                                name: m['goodsName'],
+                                image: m['image'],
+                                count: count,
+                                price: m['presentPrice']));
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext ctx) {
+                                  return CartIosPage();
+                                },
+                              ),
+                            );
+                          },
+                          child: Text(
+                            '确认加入购物车',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
+      onWillPop: () {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => IndexPage()),
+            (route) => route == null);
+      },
     );
   }
 
