8eb69cb3f0338fb5a8c53a5c15bcd0abcceeda67
lujunyu <toplu1015@163.com>
2019-03-14 09:20:19 +0800

    main

--- baixin/lib/main.dart
+++ baixin/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:amap_base/amap_base.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_button/db/db_helper.dart';
-import 'package:flutter_button/model/cart_model.dart';
-import 'package:flutter_button/page/index_main.dart';
-import 'package:flutter_button/pageOne.dart';
 import 'package:flutter_button/widget/loading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/services.dart';
