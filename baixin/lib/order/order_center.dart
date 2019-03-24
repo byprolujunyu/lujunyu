bfa60af421ad5ecbf7bfad74fc6cb3a897c4b232
lujunyu <toplu1015@163.com>
2019-03-24 11:59:52 +0800

    删除多余

--- baixin/lib/order/order_center.dart
+++ baixin/lib/order/order_center.dart
@@ -1,19 +1,6 @@
-import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_button/constants/index.dart';
-import 'package:flutter_button/detail/datail_page_new.dart';
-import 'package:flutter_button/detail/detail_page.dart';
-import 'package:flutter_button/fuji/youhui_pic.dart';
-import 'package:flutter_button/page/category_page.dart';
-import 'package:flutter_button/page/index_main.dart';
-import 'package:flutter_button/page/seracher.dart';
-import 'package:flutter_button/shop/shop_info.dart';
-import 'package:flutter_button/utils/loading_progress.dart';
-import 'package:flutter_button/widget/ada_widget.dart';
-import '../service/service_method.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:flutter_easyrefresh/easy_refresh.dart';
+import '../constants/index.dart';
 
 class OrderCenterPage extends StatefulWidget {
   final int index;
